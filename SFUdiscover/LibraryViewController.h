f07db7406eec8ac61b56031b7a19fb07836a65f3
Clayton Jian <claytonj@wmfmac21.ucslabs.sfu.ca>
2015-02-28 17:57:57 -0800

    Updated home screen to make it scrollable, added new Library button, and navigation bars for every feature

---
+++ SFUdiscover/LibraryViewController.h
@@ -0,0 +1,13 @@
+//
+//  LibraryViewController.h
+//  SFUdiscover
+//
+//  Created by Clayton Jian on 2/28/15.
+//  Copyright (c) 2015 EngagingFoundations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LibraryViewController : UIViewController
+
+@end
