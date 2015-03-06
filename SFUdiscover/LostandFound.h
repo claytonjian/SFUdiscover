0c6e9ea4560208e20d61d31dfec21f4da813ec18
James Voong <jamesvoong@jamess-mac-pro.vc.shawcable.net>
2015-03-05 16:48:36 -0800

    Added View Controllers

---
+++ SFUdiscover/LostandFound.h
@@ -0,0 +1,16 @@
+//
+//  LostandFound.h
+//  sfudiscoverjames
+//
+//  Created by James Voong on 3/1/2015.
+//  Copyright (c) 2015 JamesVoong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LostandFound : UIViewController{
+    IBOutlet UIScrollView *scroller;
+}
+
+
+@end
---
+++ SFUdiscover/LostandFound.m
@@ -0,0 +1,51 @@
+//
+//  LostandFound.m
+//  sfudiscoverjames
+//
+//  Created by James Voong on 3/1/2015.
+//  Copyright (c) 2015 JamesVoong. All rights reserved.
+//
+
+#import "LostandFound.h"
+
+@interface LostandFound ()
+
+@end
+
+@implementation LostandFound
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [scroller setScrollEnabled:YES];
+    [scroller setContentSize:CGSizeMake(320, 568)];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
