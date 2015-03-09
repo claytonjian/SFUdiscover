45ee86a05de44074c7992362f8f8ee9235d049ac
Clayton Jian <claytonj@wmfmac05.ucslabs.sfu.ca>
2015-03-09 15:38:48 -0700

    Added headers for Bonnie’s files

--- SFUdiscover/EventManager.h
+++ SFUdiscover/EventManager.h
@@ -3,6 +3,14 @@
 //  EventKitDemo
 //
 //  Created by Bonnie Ha on 2015-03-06.
+//  Contributors: Bonnie Ha, James Voong
+//
+//  - Created and added needed objects (Bonnie)
+//  - Go back and go home ibactions implemented (James)
+//  - Button borders changed (Riku)
+//
+//  Known Bugs:
+//
 // Modified from http://www.appcoda.com/ios-event-kit-programming-tutorial/
 //  Copyright (c) 2015 EngagingFoundations. All rights reserved.
 //
